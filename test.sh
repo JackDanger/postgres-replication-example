0450a08a70e707ea4bcc3bbd9d78ce70893ee553
Jack Danger <github@jackcanty.com>
2020-06-20 19:10:40 -0700

    Writing replication plan

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create two upstream databases
+
+# Create existing upstream data
+
+# Create single downstream database
+
+# Begin replication
+
+# Check downstream for existing data
+
+# Insert new data upstream
+
+# Check downstream for existing data
